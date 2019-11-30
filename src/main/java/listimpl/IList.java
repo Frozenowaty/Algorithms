1768cd59411f2bb5d6448b9255d69b80df41e5be
Sławoj Wiśniewski <slawoj.wis@gmail.com>
2019-11-30 12:40:49 +0100

    List Implementations

---
+++ src/main/java/listimpl/IList.java
@@ -0,0 +1,12 @@
+package listimpl;
+
+public interface IList <T> {
+    boolean isEmpty();
+    int size();
+    void add(T value);
+    void add(T value, int index);
+    T get(int index);
+    void set(T value, int index);
+    void remove(int index);
+
+}
